596d97ec611f35b5a8dbc5aedb99c9a796f630a4
Ayham Alhelal <a_alhelal@hotmail.com>
2024-07-09 23:36:47 +0200

    Create netlify file to deploy

---
+++ netlify/functions/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const appRoute = require('../../routes/route.js');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require( 'path' );
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// Use the router for the root path
+app.use('/', appRoute);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/** routes */
+app.use('/api', appRoute);
+
+// Handle 404 errors by redirecting to the root path
+app.use((req, res, next) => {
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
+module.exports.handler = serverless(app);
